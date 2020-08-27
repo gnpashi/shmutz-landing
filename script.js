d5e2af6c19d26546e328f093359a461512b3574a
Pashi <guynatan@gmail.com>
2020-08-27 16:46:16 +0300

    select2

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+  $('#select_city').select2();
 	let optionList = document.getElementById('select_city').options;
-	let options = [{text: "בחרו ישוב", value: ""}];
-	var count = 0
+	let options = [{text: "בחרו ישוב", value: "", region:""}];
+	let regions = ["g"]
 	var data = {
 		 resource_id: '5c78e9fa-c2e2-4771-93ff-7f400a12f7ba', // the resource id
 		 limit: 1273, // get 5 results
@@ -13,18 +14,28 @@ $(document).ready(function() {
 		 // dataType: 'text',
 		 success: function(data) {
 			 for (var i = 0; i < data.result.records.length; i++) {
-				 var opt = {text: data.result.records[i]["שם_ישוב"].trim(), value: data.result.records[i]["שם_ישוב"].trim()}
+				 var opt = {text: data.result.records[i]["שם_ישוב"].trim(),
+				  					value: data.result.records[i]["שם_ישוב"].trim(),
+					 					region: data.result.records[i]["שם_נפה"].trim()}
 				 options.push(opt)
+				 if (!regions.includes(opt.region)) regions.push(opt.region)
 				 // console.log(opt);
-				 count++
 			 	// console.log(data.result.records[i]["שם_ישוב"])
 			 }
-			 console.log(count);
 			 options.forEach(option =>
 				 optionList.add(
 					 new Option(option.text, option.value)
 				 )
 			 );
+			 console.log(regions);
+			 // console.log(options[2]);
+			 // console.log(region);
+			 $("#select_city").change(function(event) {
+				 var city = $(this).val()
+				 var region = options.find(option => option.text === city).region;
+				 console.log(region);
+				 $("#region").val(region)
+			 });
 		 }
 	 });
 });
